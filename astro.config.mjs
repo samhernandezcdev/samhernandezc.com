359d20a526466b5245d348312751d264a81e5cd6
samhernandezcdev <samhernandezc.dev@proton.me>
2026-06-10 17:18:14 -0600

    [ADD] [Footer] Modificando el Hero, y agregando el Footer

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 
 import react from '@astrojs/react';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://samhernandezc.bkode.lat",
-  integrations: [react()],
+  integrations: [react(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
